6000336601e7f10aaf731d9f2d50dfd106b2617d
ConocchiariRiccardo <163643671+ConocchiariRiccardo@users.noreply.github.com>
2026-04-02 16:49:28 +0200

    aggiunta classe viaggio e utente

---
+++ lib/domain/models/viaggio.dart
@@ -0,0 +1,21 @@
+class Viaggio{
+    final String id;
+    final String nome;   // Nome del viaggio
+    final String destinazione;   // Destinazione del viaggio
+    final DateTime dataPartenza;   // Data di partenza del viaggio
+    final DateTime dataRitorno;   // Data di ritorno del viaggio
+    final String descrizione;   // Descrizione del viaggio
+    final String immagineUrl;   // URL dell'immagine rappresentativa del viaggio
+    final bool completato;   // Indica se il viaggio è stato completato o meno
+
+    Viaggio({
+        required this.id,
+        required this.nome,
+        required this.destinazione,
+        required this.dataPartenza,
+        required this.dataRitorno,
+        required this.descrizione,
+        required this.immagineUrl,
+        this.completato = false,
+    });
+}
